b3037e8643f7eba07c7f9939f22687e287d0e2aa
armacode3 <zavaarmando0@gmail.com>
2025-10-14 20:01:31 -0400

    CMake implemented

---
+++ build/simulation/CMakeFiles/simulation_engine.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/zavaarma/workspace/github.com/Flight-Sensor-Simulator/simulation/flight_computer.cpp" "simulation/CMakeFiles/simulation_engine.dir/flight_computer.cpp.o" "gcc" "simulation/CMakeFiles/simulation_engine.dir/flight_computer.cpp.o.d"
+  "/home/zavaarma/workspace/github.com/Flight-Sensor-Simulator/simulation/physics_engine.cpp" "simulation/CMakeFiles/simulation_engine.dir/physics_engine.cpp.o" "gcc" "simulation/CMakeFiles/simulation_engine.dir/physics_engine.cpp.o.d"
+  "/home/zavaarma/workspace/github.com/Flight-Sensor-Simulator/simulation/sensors.cpp" "simulation/CMakeFiles/simulation_engine.dir/sensors.cpp.o" "gcc" "simulation/CMakeFiles/simulation_engine.dir/sensors.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/simulation/CMakeFiles/simulation_engine.dir/flight_computer.cpp.o.d
@@ -0,0 +1,5 @@
+simulation/CMakeFiles/simulation_engine.dir/flight_computer.cpp.o: \
+ /home/zavaarma/workspace/github.com/Flight-Sensor-Simulator/simulation/flight_computer.cpp \
+ /usr/include/stdc-predef.h \
+ /home/zavaarma/workspace/github.com/Flight-Sensor-Simulator/simulation/flight_computer.h \
+ /home/zavaarma/workspace/github.com/Flight-Sensor-Simulator/simulation/constants.h
