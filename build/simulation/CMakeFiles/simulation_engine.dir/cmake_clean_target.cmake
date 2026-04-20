84849b4b277e1b4d51821e092f9bb769fd8d3f18
Armando <zavaarmando0@gmail.com>
2026-04-20 13:22:52 -0400

    Delete build directory

--- build/simulation/CMakeFiles/simulation_engine.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libsimulation_engine.a"
-)
--- build/simulation/CMakeFiles/simulation_engine.dir/flight_computer.cpp.o.d
+++
@@ -1,5 +0,0 @@
-simulation/CMakeFiles/simulation_engine.dir/flight_computer.cpp.o: \
- /home/zavaarma/workspace/github.com/Flight-Sensor-Simulator/simulation/flight_computer.cpp \
- /usr/include/stdc-predef.h \
- /home/zavaarma/workspace/github.com/Flight-Sensor-Simulator/simulation/flight_computer.h \
- /home/zavaarma/workspace/github.com/Flight-Sensor-Simulator/simulation/constants.h
